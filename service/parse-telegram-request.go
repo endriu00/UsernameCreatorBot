a548e101549665065308726bf8af96cef99dc580
Andrea Stella <andreaste00@gmail.com>
2021-08-28 19:01:29 +0200

    fixing error in update

--- service/parse-telegram-request.go
+++ service/parse-telegram-request.go
@@ -10,7 +10,8 @@ func ParseTelegramRequest(r *http.Request) (*Update, error) {
 	var update Update
 	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
 		log.Printf("Incoming update cannot be decoded! %s", err.Error())
-		log.Print(r.Body)
+		log.Print(update)
+		log.Print(&update)
 
 	}
 	return &update, nil
