d414e0dc37a2c1d20237520a8bcfbbd33ee15157
Andrea Stella <andreaste00@gmail.com>
2021-08-31 22:04:30 +0200

    Fixed wrong message in Info

--- service/pack-info.go
+++ service/pack-info.go
@@ -2,10 +2,10 @@ package service
 
 func PackInfo(message Message) string {
 	infoMessage := "Hey @" + message.From.Username + "\n" +
-		"I am nomebot, your username creator bot!" +
+		"I am nomebot, your username creator bot!\n" +
 		"To start with your username creation, just type\n" +
 		"/generate `yourname`\n" +
-		"where `yourname` is your name, your username starting point" +
+		"where `yourname` is your name, your username starting point " +
 		"or anything else.\n" +
 		"Feel free to ask me for info again whenever you want with `/info` !"
 	return infoMessage
